93d352318ab96dfb283f46fa330ad5586b906c59
emilio <ediaz-goico@me.bergen.edu>
2020-07-21 17:10:40 -0400

    finished searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,9 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(len(arr)): 
+
+        if arr[i] == target: 
+            return i 
 
 
     return -1   # not found
@@ -9,6 +13,15 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
+    start = 0
+    end = len(arr) - 1
+    while   start <= end:
+        midindex = (end + start) // 2
+        if target == arr[midindex]:
+            return midindex
+        if target < arr[midindex]:
+            end = midindex - 1
+        if target > arr[midindex]:
+            start = midindex + 1
 
     return -1  # not found
